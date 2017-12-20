120c65cd57f5b5d37173ae3540991b50bcbe2569
Vitor Baptista <vitor@vitorbaptista.com>
2017-12-20 13:41:10 +0000

    Refactor tests to use pytest

--- test_profiles.py
+++ test_profiles.py
@@ -1,11 +1,7 @@
 import os
 import json
+import pytest
 from jsonschema import validate
-
-PROFILES = [
-    'assets/trees/trees-data-package.json',
-    'assets/grants/grants-data-package.json',
-]
 
 
 def find_profiles():
@@ -17,14 +13,16 @@ def find_profiles():
                 profiles.append(os.path.join(path[0], path[1], _file))
     return profiles
 
-for profile in find_profiles():
-    base_dir = os.path.dirname(profile)
 
-    data_package_descriptor = json.load(
-        open(os.path.join(base_dir, 'datapackage.json'), 'r'))
+@pytest.mark.parametrize('profile_path', find_profiles())
+def test_profile(profile_path):
+    base_dir = os.path.dirname(profile_path)
+    datapackage_path = os.path.join(base_dir, 'datapackage.json')
 
-    json_schema = json.load(open(profile, 'r'))
+    with open(datapackage_path, 'r') as fp:
+        datapackage_descriptor = json.load(fp)
 
-    validate(data_package_descriptor, json_schema)
+    with open(profile_path, 'r') as fp:
+        jsonschema = json.load(fp)
 
-    print('Validation for profile {} OK'.format(profile))
+    validate(datapackage_descriptor, jsonschema)
